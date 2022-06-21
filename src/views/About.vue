1258999aaef0532ec0f451ef582d055d8b71e644
armandomercado <jmercado@pasteursoft.com>
2022-06-20 22:04:35 -0500

    app para registro de datos en mongo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,122 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Lista de productos</h1>
+    <div>
+      <table class="tb">
+        <tr>
+          <th>Nombre</th>
+          <th>Vendedor</th>
+          <th>Descripcion</th>
+          <th>Precio</th>
+          <th>ID stripe</th>
+        </tr>
+        <tr v-for="p in products" :key="p._id">
+          <td>{{p.name}}</td>
+          <td>{{p.company_name}}</td>
+          <td>{{p.description}}</td>
+          <td>$ {{p.price}}.00 MXN</td>
+          <td>{{p.stripe_price}}</td>
+        </tr>
+      </table>
+    </div>
+    <h1>Registar nuevo producto</h1>
+    <div>
+      <div class="form-item">
+        <label class="form-label">Nombre del producto</label>
+        <input type="text" v-model="new_product.name"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">Descripcion del producto</label>
+        <input type="text" v-model="new_product.description"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">Nombre del vendedor</label>
+        <input type="text" v-model="new_product.company_name"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">ID del vendedor</label>
+        <input type="text" v-model="new_product.company_id"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">URL imagen del producto</label>
+        <input type="text" v-model="new_product.image"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">Precio en numero</label>
+        <input type="number" v-model="new_product.price"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">Precio ID stripe</label>
+        <input type="text" v-model="new_product.stripe_price"/>
+      </div>
+      <div class="form-item">
+        <input type="button" value="Registrar producto" v-on:click="postProduct"/>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return{
+      products: null,
+      new_product: {
+        name: '',
+        company_id: '',
+        description: '',
+        image: '',
+        company_name: '',
+        price: 0,
+        stripe_price: ''
+      }
+    }
+  },
+  created(){
+    this.getProducts()
+  },
+  methods:{
+    getProducts() {
+      axios.get('http://localhost:3000/api/products')
+      .then(response =>{
+        this.products = response.data;
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+    },
+    postProduct(){
+      axios.post('http://localhost:3000/api/products/new', this.new_product)
+      .then(response =>{
+        console.log(response.data);
+        this.getProducts();
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tb {
+  border: 1px solid;
+
+}
+
+td {
+  border: 1px solid;
+  padding: 5px;
+}
+
+.form-item {
+  padding: 10px;
+}
+
+.form-label{
+  padding: 20px; 
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,107 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Lista de Empresas</h1>
+    <div>
+      <table class="tb">
+        <tr>
+          <th>Nombre</th>
+          <th>Categoria</th>
+          <th>Descripcion</th>
+        </tr>
+        <tr v-for="c in companies" :key="c._id">
+          <td>{{c.company_name}}</td>
+          <td>{{c.category}}</td>
+          <td>{{c.description}}</td>
+        </tr>
+      </table>
+    </div>
+    <h1>Registar nueva empresa</h1>
+    <div>
+      <div class="form-item">
+        <label class="form-label">Nombre de la empresa</label>
+        <input type="text" v-model="new_company.company_name"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">Descripcion de la empresa</label>
+        <input type="text" v-model="new_company.description"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">Categoria</label>
+        <input type="text" v-model="new_company.category"/>
+      </div>
+      <div class="form-item">
+        <label class="form-label">URL logo de la empresa</label>
+        <input type="text" v-model="new_company.logo"/>
+      </div>
+      <div class="form-item">
+        <input type="button" value="Registrar producto" v-on:click="postCompany"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      companies: null,
+      new_company: {
+        company_name: '',
+        description: '',
+        category: '',
+        logo: ''
+      }
+    }
+  },
+  created(){
+    this.getCompanies();
+  },
+  methods: {
+    getCompanies(){
+      axios.get('http://localhost:3000/api/companies')
+      .then(response =>{
+        this.companies = response.data;
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+    },
+    postCompany(){
+      axios.post('http://localhost:3000/api/companies/new', this.new_company)
+      .then(response =>{
+        console.log(response.data);
+        this.getCompanies();
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+.tb {
+  border: 1px solid;
+
+}
+
+td {
+  border: 1px solid;
+  padding: 5px;
+}
+
+.form-item {
+  padding: 10px;
+}
+
+.form-label{
+  padding: 20px; 
+}
+</style>
